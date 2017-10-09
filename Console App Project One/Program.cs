e570184e24e6e14a523ed81104224497f2a4807a
Darrell Stovall <darrell.stovall.trussville@gmail.com>
2017-10-09 13:04:50 -0500

    Initial Adding of Files

---
+++ Console App Project One/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Console_App_Project_One
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User();
+
+            Console.WriteLine("Welcome to the most awesome bank ever.  In order to use your money with us, you'll have to give us some information.");
+
+            user.FirstName = AskQuestion("What is your first name?");
+            Console.WriteLine("Great! Your name is " + user.FirstName + ".");
+
+            user.LastName = AskQuestion("What is your last name?");
+            Console.WriteLine("Right. So your full name is " + user.FirstName + " " + user.LastName + ".");
+
+            user.IsAccountOwner = AskBoolQuestion("Are you the account owner?");
+            Console.WriteLine("Cool. Account owner: " + user.IsAccountOwner);
+
+            user.PinNumber = AskPinNumber("What would you like for your 4-digit Pin Number to be?", 4);
+            Console.WriteLine("Yes, we can use that.  Keep this for your records:  " + user.PinNumber);
+                
+            //This is only needed when debugging. We can remove the below line when published.
+            Console.ReadLine();
+        }
+
+        static string AskQuestion(string question)
+        {
+            Console.WriteLine(question);
+            return Console.ReadLine();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="question">A question to be asked to the console.</param>
+        /// <param name="limit">We should limit the pinNUmber to 4 digits.</param>
+        /// <returns></returns>
+        static string AskPinNumber(string question, int length)
+        {
+            string numberString = null;
+
+            while (numberString == null)
+            {
+                var response = AskQuestion(question);
+
+                if (response.Length == length && Int32.TryParse(response, out int _))
+                {
+                    numberString = response;
+                }
+            }
+
+            return numberString;
+        }
+
+        static bool AskBoolQuestion(string question)
+        {
+            var hasAnswered = false;
+            var responseBool = false;
+
+            while (!hasAnswered)
+            {
+                var response = AskQuestion(question + " (y/n)");
+
+                if (response == "y")
+                {
+                    responseBool = true;
+                    hasAnswered = true;
+                }
+                else if (response == "n")
+                {
+                    responseBool = false;
+                    hasAnswered = true;
+                }
+            }
+
+            return responseBool;
+        }
+    }
+}
---
+++ Console App Project One/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Console_App_Project_One
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsAccountOwner { get; set; }
+        public string PinNumber { get; set; }
+    }
+}
